b1ea5d1b206023b0d6cfb9bba897bb4a0f1fb49a
AlmeidaGeovanna <irisg7983@gmail.com>
2025-09-04 21:53:50 -0300

    Treinando programação com html e javascript

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert("Olá Mundo!")
+// alert("Olá Mundo!")
+// alert("Oi")
+// alert("Tudo bem?")
+// alert("Você vem sempre aqui?")
+
+// let idade
+// idade = 18
+// alert(idade)
+// idade = "melancia"
+// alert(idade)
+// idade = 18.1
+// alert(idade)
+
+const amor = "meu primeiro amor"
+alert(amor)
+
+alert(amor)
+let amor2 = "meu segundo amor"
+
+var antigao = "Nunca usar"
+
+
+
+
+
+
