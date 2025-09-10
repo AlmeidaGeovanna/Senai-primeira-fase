3b7be3929a8a6029f246ae73224f4cf7db53cd3d
AlmeidaGeovanna <irisg7983@gmail.com>
2025-09-09 22:14:18 -0300

    variaveis em javascript

---
+++ Iris/logica-js/variaveis.js
@@ -0,0 +1,23 @@
+//variável com var
+// var nome = "Iris"
+// var nome = "João"
+// nome = "João"
+
+//variável com let
+// let nome = "Iris"
+// nome = "João"
+
+//variável com const
+const nome = "Iris" //string
+const sobrenome = "Silva" //string
+const idade = 16 //number
+const feminino = true //boolean
+
+//array
+const frutas = ["laranja","maçã","banana"]
+console.log(frutas[0])
+
+
+// console.log("Meu nome é " + nome + " " + sobrenome + " e minha idade é " + idade + " anos")
+
+// console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`)
