908e81a32a87a751234d47de508980a825af4b6d
AlmeidaGeovanna <irisg7983@gmail.com>
2025-09-10 22:07:33 -0300

    exercicios de javascript

---
+++ Iris/logica-js/sq02/ex01.js
@@ -0,0 +1,30 @@
+// let idade //sq02 - 1
+// idade = Number ( prompt ("Digite sua idade: "))
+// if(idade >=18){
+//     alert("Maior de idade")
+// }else{
+//     alert("Menor de idade")
+// }
+// console.log(idade);
+
+
+let numeroAleatorio
+let tentativa
+numeroAleatorio = Number (prompt ("Digite um número: "))
+tentativa = Number (prompt("Tente adivinhar o número aleatório: "))
+if (numeroAleatorio == tentativa) {
+    alert("Você acertou!")
+}else{
+    alert("Você errou")
+}
+
+
+
+
+// let numero //sq02 - 4
+// numero = Number (prompt ("Digite um número: "))
+// if (numero  % 2 == 0){
+//       alert("Número par")
+// }else{
+//     alert("Número ímpar")
+// }
