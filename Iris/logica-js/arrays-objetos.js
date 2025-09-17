9e8da1d42aecff18ab0b393f558acfb61f367702
AlmeidaGeovanna <irisg7983@gmail.com>
2025-09-16 22:07:15 -0300

    exercicios com arrays e objetos

---
+++ Iris/logica-js/arrays-objetos.js
@@ -0,0 +1,22 @@
+const alunosDoSenai = [
+    {
+        nome:"João",
+        email:"joao@gmail.com",
+        matricula:"123",
+        idade: 40
+    },
+    {
+        nome:"Maria",
+        email:"maria@gmail.com",
+        matricula:"321",
+        idade:30
+    },
+    {
+        nome:"Iris",
+        email:"iris@gmail.com",
+        matricula:"001",
+        idade:16
+    }
+]
+
+console.log (`Meu nome é ${alunosDoSenai[2].nome}, meu email é ${alunosDoSenai[2].email}, minha matricula é ${alunosDoSenai[2].matricula} e eu tenho ${alunosDoSenai[2].idade} anos`)
