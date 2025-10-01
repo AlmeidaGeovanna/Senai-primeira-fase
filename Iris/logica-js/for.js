3c070f7be719293ef314f3082ec88cbdcec2ef5b
AlmeidaGeovanna <irisg7983@gmail.com>
2025-09-30 21:47:14 -0300

    exercicios

---
+++ Iris/logica-js/for.js
@@ -0,0 +1,29 @@
+// for(let i = 0; i<=10; i++){
+//     console.log(i)
+// }
+
+// for(let i = 10; i>=0; i--){
+//     console.log(i)
+// }
+
+// for(let i = 2; i<=20; i+=2){
+//     console.log(i)
+// } //(+= número = número + x)
+
+// let soma = 0
+// for(let i = 1; i<100; i++){
+//     soma += i
+// }
+// console.log("resultado da soma",soma)
+
+// let alunos = ["João","Maria","Joana"]
+// alunos.push("Pedro")
+// for(let i = 0; i<alunos.length; i++){
+//     console.log("aluno: ", alunos[i])
+// }
+// console.log(alunos.length)
+
+// let frutas = ["Banana" , "Caju" , "Melancia" , "Melão" , "Laranja"]
+// for(let i = 0; i<frutas.length; i++){
+//     console.log(`fruta ${[i+1]}:`, frutas[i])
+// }
