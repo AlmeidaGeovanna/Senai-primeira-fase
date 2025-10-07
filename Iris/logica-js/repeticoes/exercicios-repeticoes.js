3d18f9c433030eb555e22fa2c3c4083218e9a56e
AlmeidaGeovanna <irisg7983@gmail.com>
2025-10-06 22:01:25 -0300

    exercicios

--- Iris/logica-js/repeticoes/exercicios-repeticoes.js
+++ Iris/logica-js/repeticoes/exercicios-repeticoes.js
@@ -1,13 +1,48 @@
+//ex01 - parte 1 
 // for(let i = 1; i<=10; i++){
 //     let quadrado = i*i
 //     alert("O quadrado é: " + quadrado)
 // }
 
+//ex02 - parte 1 
 // for(let i = 10; i>=0; i--){
 //     alert(i)}
 
+//ex03 - parte 1 
 // let soma = 0
 // for(let i = 1; i<=100; i++){
 //     soma = soma + i
 //     alert("A soma é: " + soma)
-// }   
+// }  
+
+//ex04 - parte 1
+// for (let i = 1; i <= 10; i++) {
+//      alert("5 x " + i + " = " + (5 * i));
+// }
+
+//ex05 - parte 1
+// let numero = Number(prompt("Digite um número: "))
+// for (let i = 1; i <= 10; i++){
+//     alert( numero + " x " + i + " = " + (numero * i));
+// }  
+// alert("Tabuada: " + tabuada)
+
+//ex06 - parte 1
+// for(let i = 1; i <= 20; i++) {
+//     if(i % 2 == 1){
+//     alert("Número: " + i)
+// }}
+
+//ex07 - parte 1
+// for(let i = 1; i<=10; i++){
+// alert(i)}
+// for(let i = 10; i>=1; i--){
+// alert(i)}
+
+//ex08 - parte 1
+// let fatorial = 1
+// let numero = Number(prompt("Digite um número: "))
+// for(let i = 1; i<=numero; i++){
+//     fatorial = fatorial * i
+//     alert("O fatorial é: " + fatorial)
+// }
