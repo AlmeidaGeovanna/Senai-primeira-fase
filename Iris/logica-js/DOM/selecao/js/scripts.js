a827081f958f00151ed270a1e049da6c4e091fbb
AlmeidaGeovanna <irisg7983@gmail.com>
2025-11-18 20:40:09 -0300

    exercicios

---
+++ Iris/logica-js/DOM/selecao/js/scripts.js
@@ -0,0 +1,9 @@
+//busca
+
+const inputForm = document.querySelector("#busca")
+const botaoBusca = document.querySelector ("#botaoBusca")
+const resultadoDiv = document.querySelector("#resultado")
+
+inputForm. addEventListener("keydown",(e)=>{
+    console.log(e.key)
+})
